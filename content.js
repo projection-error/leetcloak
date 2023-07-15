6074f06578bf7ad5d9de6fd8263113ce22fa20c8
projection-error <84434778+projection-error@users.noreply.github.com>
2023-07-14 20:09:02 -0700

    Update content.js
    Updates to accommodate recent CSS changes to the leetcode website.

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 const dif_class = [
-  '[class="bg-olive dark\\:bg-dark-olive text-olive dark\\:text-dark-olive inline-block rounded-\\[21px\\] bg-opacity-\\[\\.15\\] px-2\\.5 py-1 text-xs font-medium capitalize dark\\:bg-opacity-\\[\\.15\\]"]',
-  '[class="bg-yellow dark\\:bg-dark-yellow text-yellow dark\\:text-dark-yellow inline-block rounded-\\[21px\\] bg-opacity-\\[\\.15\\] px-2\\.5 py-1 text-xs font-medium capitalize dark\\:bg-opacity-\\[\\.15\\]"]',
-  '[class="bg-pink dark\\:bg-dark-pink text-pink dark\\:text-dark-pink inline-block rounded-\\[21px\\] bg-opacity-\\[\\.15\\] px-2\\.5 py-1 text-xs font-medium capitalize dark\\:bg-opacity-\\[\\.15\\]"]'
+  'text-olive',
+  'text-yellow',
+  'text-pink'
 ];
 
 const targ_class = 'dontpanic dark:bg-dark-olive text-white dark:text-dark-olive inline-block rounded-[21px] bg-opacity-[.15] px-2.5 py-1 text-xs font-medium capitalize dark:bg-opacity-[.15]';
@@ -9,19 +9,20 @@ const targ_class = 'dontpanic dark:bg-dark-olive text-white dark:text-dark-olive
 function replaceTargetDiv(targetDiv) {
   const newDiv = document.createElement('div');
   newDiv.className = targ_class;
-  newDiv.textContent = "U Got This "+"\uD83D\uDC4D";
+  newDiv.textContent = "U Got This " + "\uD83D\uDC4D";
   targetDiv.parentNode.replaceChild(newDiv, targetDiv);
 }
 
 // Delayed execution using setTimeout
 setTimeout(function() {
-  const targetDivs = document.querySelectorAll(dif_class.join(', '));
-  if (targetDivs.length > 0) {
-    // Replace each target div
-    targetDivs.forEach(targetDiv => {
-      replaceTargetDiv(targetDiv);
-    });
-  } else {
-    console.log('Target divs not found.');
-  }
+  dif_class.forEach(classname => {
+    const targetDivs = Array.from(document.getElementsByClassName(classname));
+    if (targetDivs.length > 0) {
+      targetDivs.forEach(targetDiv => {
+        replaceTargetDiv(targetDiv);
+      });
+    } else {
+      console.log('Target divs not found for class:', classname);
+    }
+  });
 }, 300); // Adjust the delay as needed (in milliseconds)
